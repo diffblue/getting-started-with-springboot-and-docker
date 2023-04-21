cddb48d6a9d9e081031a89d9b44dd257b2958539
Rajkumar Venkatasamy <rajkumar.venkatasamy@navis.com>
2023-04-21 07:59:55 +0530

    Replaced ContextConfiguration and ExtendWith annotation with SpringBootTest annotation

--- src/test/java/com/example/gettingstartedwithspringbootanddocker/service/ProductsServiceTest.java
+++ src/test/java/com/example/gettingstartedwithspringbootanddocker/service/ProductsServiceTest.java
@@ -14,14 +14,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ContextConfiguration(classes = {ProductsService.class})
-@ExtendWith(SpringExtension.class)
+
+@SpringBootTest
 class ProductsServiceTest {
     @MockBean
     private ProductsRepository productsRepository;
